c060fbce4b4078d93ae00775d4dd4ad04427ffab
Chase Lancaster <chaseblancaster@gmail.com>
2020-10-29 18:18:07 -0700

    require fs and then readFile of santa.text in question1  function

---
+++ script.js
@@ -0,0 +1,17 @@
+// From https://adventofcode.com/2015/day/1
+
+const fs = require('fs')
+
+// 1 - What floor does Santa end on?
+// ( --> should go up one floor
+// ) --> should go down one floor
+
+const question1 = () => {
+    fs.readFile('./santa.text', (err, data) => {
+        const directions = data.toString()
+    })
+}
+
+
+
+// 2 - When does Santa first enter the basement?
